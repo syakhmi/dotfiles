f2411e8d23430fa260de2c7363f034e73697b893
Sahil Yakhmi <sahil@cs.stanford.edu>
2023-12-22 16:05:26 -0500

    Don't try to install zsh from source on Linux and don't use git protocol

--- dependencies.sh
+++ dependencies.sh
@@ -27,23 +27,14 @@ if ! command_exists zsh; then
       brew install zsh
     fi
   else
-    (
-    cd
-    wget -O zsh-5.0.2.tar.gz http://downloads.sourceforge.net/project/zsh/zsh/5.0.2/zsh-5.0.2.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fzsh%2Ffiles%2Fzsh%2F5.0.2%2F
-    tar -xvf zsh-5.0.2.tar.gz
-    rm zsh-5.0.2.tar.gz
-    cd zsh-5.0.2
-    ./configure --enable-pcre
-    make
-    sudo make install
-    )
-    check_status
+    echo "Please install zsh:"
+    false
   fi
 fi
 
-conditional_clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+conditional_clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 conditional_clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
 conditional_clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-conditional_clone git://github.com/tarruda/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+conditional_clone https://github.com/tarruda/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 relink $DOTFILES/sahil.zsh-theme ~/.oh-my-zsh/themes/sahil.zsh-theme
